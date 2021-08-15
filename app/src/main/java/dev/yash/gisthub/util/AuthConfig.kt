5ea837a55dac620fb02561f66f77a7f1f076dd89
Yash-Garg <yash.garg@outlook.in>
2021-08-15 19:36:31 +0530

    feat: add oauth impl and print result for now
    Signed-off-by: Yash-Garg <yash.garg@outlook.in>

--- app/src/main/java/dev/yash/gisthub/util/AuthConfig.kt
+++ app/src/main/java/dev/yash/gisthub/util/AuthConfig.kt
@@ -1,11 +1,22 @@
 package dev.yash.gisthub.util
 
 import android.net.Uri
+import net.openid.appauth.AuthorizationRequest
 import net.openid.appauth.AuthorizationServiceConfiguration
+import net.openid.appauth.ResponseTypeValues
 
-class AuthConfig {
+object AuthConfig {
     private val serviceConfiguration = AuthorizationServiceConfiguration(
         Uri.parse("https://github.com/login/oauth/authorize"),
         Uri.parse("https://github.com/login/oauth/access_token")
     )
+
+    private val authRequestBuilder = AuthorizationRequest.Builder(
+        serviceConfiguration,
+        Secrets.CLIENT_ID,
+        ResponseTypeValues.TOKEN,
+        Uri.parse(Secrets.REDIRECT_URI),
+    )
+
+    val authRequest = authRequestBuilder.setScope("gist").build()
 }
