cab1489c9739856a393c989edcf43e1c5d734f7a
Yash-Garg <yash.garg@outlook.in>
2021-08-15 13:46:02 +0530

    cleanup: remove toast and define proper activity

--- app/src/main/java/dev/yash/gisthub/MainActivity.kt
+++ app/src/main/java/dev/yash/gisthub/MainActivity.kt
@@ -1,7 +1,6 @@
 package dev.yash.gisthub
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import app.yash.gisthub.databinding.AuthActivityBinding
 
@@ -11,10 +10,5 @@ class MainActivity : AppCompatActivity() {
 
         val binding = AuthActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val authButton = binding.signinButton
-        authButton.setOnClickListener {
-            Toast.makeText(this, "Please wait... Signing in", Toast.LENGTH_LONG).show()
-        }
     }
 }
