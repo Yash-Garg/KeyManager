8dee0cc50f0c0b01a3497774a1096b52a4ad8a2e
Yash-Garg <yash.garg@outlook.in>
2021-08-16 14:47:31 +0530

    chore: move auth actions to auth activity and some misc. changes

--- app/src/main/java/dev/yash/keymanager/MainActivity.kt
+++ app/src/main/java/dev/yash/keymanager/MainActivity.kt
@@ -2,62 +2,27 @@ package dev.yash.keymanager
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import app.yash.keymanager.databinding.ActivityMainBinding
-import dev.yash.keymanager.util.AuthConfig
-import dev.yash.keymanager.util.Secrets
+import app.yash.keymanager.databinding.MainActivityBinding
+import dev.yash.keymanager.ui.AuthActivity
 import dev.yash.keymanager.util.SharedPrefs
-import net.openid.appauth.AuthorizationResponse
-import net.openid.appauth.AuthorizationService
-import net.openid.appauth.ClientSecretBasic
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var authService: AuthorizationService
+    private val prefs = SharedPrefs.getEncryptedSharedPreferences(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val loginButton = binding.signinButton
-        authService = AuthorizationService(this)
-
-        loginButton.setOnClickListener {
-            val authIntent = authService.getAuthorizationRequestIntent(AuthConfig.authRequest)
-            getAuthCodeFromResult.launch(authIntent)
+        val token = prefs.getString("ACCESS_TOKEN", null)
+        if (token == null) {
+            val intent = Intent(this, AuthActivity::class.java)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, token, Toast.LENGTH_SHORT).show()
         }
-    }
-
-    private val getAuthCodeFromResult =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                result.data?.let { getAccessToken(it) }
-            } else {
-                Toast.makeText(this, "Error Retrieving Auth Token", Toast.LENGTH_LONG).show()
-            }
-        }
-
-    private fun getAccessToken(authIntent: Intent) {
-        val resp = AuthorizationResponse.fromIntent(authIntent)
-        val clientAuth = ClientSecretBasic(Secrets.CLIENT_SECRET)
-
-        if (resp != null) authService.performTokenRequest(
-            resp.createTokenExchangeRequest(),
-            clientAuth,
-        ) { response, exception ->
-            if (response != null) {
-                response.accessToken?.let {
-                    val prefs = SharedPrefs.getEncryptedSharedPreferences(this)
-                    prefs.edit().putString("ACCESS_TOKEN", it).apply()
-                    Toast.makeText(this, response.accessToken, Toast.LENGTH_LONG).show()
-                }
-            } else {
-                Log.e("Error", exception.toString())
-            }
-        } else Toast.makeText(this, "Error Retrieving Access Token", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/dev/yash/keymanager/ui/AuthActivity.kt
+++ app/src/main/java/dev/yash/keymanager/ui/AuthActivity.kt
@@ -1,14 +1,63 @@
 package dev.yash.keymanager.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import app.yash.keymanager.databinding.ActivityAuthBinding
+import app.yash.keymanager.databinding.AuthActivityBinding
+import dev.yash.keymanager.util.AuthConfig
+import dev.yash.keymanager.util.Secrets
+import dev.yash.keymanager.util.SharedPrefs
+import net.openid.appauth.AuthorizationResponse
+import net.openid.appauth.AuthorizationService
+import net.openid.appauth.ClientSecretBasic
 
 class AuthActivity : AppCompatActivity() {
+    private lateinit var authService: AuthorizationService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityAuthBinding.inflate(layoutInflater)
+        val binding = AuthActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val loginButton = binding.signinButton
+        authService = AuthorizationService(this)
+
+        loginButton.setOnClickListener {
+            val authIntent = authService.getAuthorizationRequestIntent(AuthConfig.authRequest)
+            getAuthCodeFromResult.launch(authIntent)
+        }
+    }
+
+    private val getAuthCodeFromResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                result.data?.let { getAccessToken(it) }
+            } else {
+                Toast.makeText(this, "Error Retrieving Auth Token", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private fun getAccessToken(authIntent: Intent) {
+        val resp = AuthorizationResponse.fromIntent(authIntent)
+        val clientAuth = ClientSecretBasic(Secrets.CLIENT_SECRET)
+
+        if (resp != null) authService.performTokenRequest(
+            resp.createTokenExchangeRequest(),
+            clientAuth,
+        ) { response, exception ->
+            if (response != null) {
+                response.accessToken?.let {
+                    val prefs = SharedPrefs.getEncryptedSharedPreferences(this)
+                    prefs.edit().putString("ACCESS_TOKEN", it).apply()
+                    Toast.makeText(this, response.accessToken, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Log.e("Error", exception.toString())
+            }
+        } else Toast.makeText(this, "Error Retrieving Access Token", Toast.LENGTH_LONG).show()
     }
 }
