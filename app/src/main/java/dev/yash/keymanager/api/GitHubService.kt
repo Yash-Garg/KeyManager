e0e55dcd6a390b99a7262315c68133329964f4c6
Yash-Garg <yash.garg@outlook.in>
2021-08-16 21:16:47 +0530

    chore: add headers and page queries to api interface

--- app/src/main/java/dev/yash/keymanager/api/GitHubService.kt
+++ app/src/main/java/dev/yash/keymanager/api/GitHubService.kt
@@ -3,13 +3,26 @@ package dev.yash.keymanager.api
 import dev.yash.keymanager.models.GpgKey
 import dev.yash.keymanager.models.SshKey
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.Query
 
 interface GitHubService {
+    @Headers("Accept: application/vnd.github.v3+json")
     @GET("user/keys")
-    suspend fun getSshKeys(): List<SshKey>
+    suspend fun getSshKeys(
+        @Header("Authorization") token: String,
+        @Query("per_page") perPage: Int = 50,
+        @Query("page") page: Int
+    ): List<SshKey>
 
+    @Headers("Accept: application/vnd.github.v3+json")
     @GET("user/gpg_keys")
-    suspend fun getGpgKeys(): List<GpgKey>
+    suspend fun getGpgKeys(
+        @Header("Authorization") token: String,
+        @Query("per_page") perPage: Int = 50,
+        @Query("page") page: Int
+    ): List<GpgKey>
 
     companion object {
         const val BASE_URL = "https://api.github.com"
