e7e9140857d0ffa8dec2607f718afe6e79ea26d4
Yash-Garg <yash.garg@outlook.in>
2021-08-21 00:11:59 +0530

    chore(ui): add bottom nav view with 2 tabs (use github logo for now)

--- app/src/main/java/dev/yash/keymanager/ui/HomeActivity.kt
+++ app/src/main/java/dev/yash/keymanager/ui/HomeActivity.kt
@@ -3,7 +3,9 @@ package dev.yash.keymanager.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import app.yash.keymanager.databinding.HomeActivityBinding
 import dagger.hilt.android.AndroidEntryPoint
 import dev.yash.keymanager.adapters.SshAdapter
@@ -25,13 +27,24 @@ class HomeActivity : AppCompatActivity() {
         val binding = HomeActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val bottomNavigationView = binding.bottomNavigationView
+        bottomNavigationView.background = null
+
         val accessToken = SharedPrefs.getAccessToken(this)
+        val progressBar = binding.loadingIndicator
         val recyclerView = binding.sshList
 
         lifecycleScope.launch {
             homeViewModel.getKeysSSH(accessToken!!).collectLatest { pagingData ->
                 recyclerView.adapter = sshAdapter
                 sshAdapter.submitData(pagingData)
+            }
+        }
+
+        lifecycleScope.launch {
+            sshAdapter.loadStateFlow.collectLatest { loadStates ->
+                progressBar.isVisible = loadStates.refresh is LoadState.Loading
+                recyclerView.isVisible = loadStates.refresh is LoadState.NotLoading
             }
         }
     }
