17e3932c63d19816a2f491308a04346adb0663aa
Yash-Garg <yash.garg@outlook.in>
2021-08-25 20:59:38 +0530

    chore: switch to stable deps and linux stuff
    Signed-off-by: Yash-Garg <yash.garg@outlook.in>

--- app/src/main/java/dev/yash/keymanager/KeyManagerApp.kt
+++ app/src/main/java/dev/yash/keymanager/KeyManagerApp.kt
@@ -4,8 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class KeyManagerApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
+class KeyManagerApp : Application() {}
