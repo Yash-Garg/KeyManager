f74f54cecc6393aaeb54575367fb60604349f026
Yash-Garg <yash.garg@outlook.in>
2021-08-30 20:54:18 +0530

    chore: use nested navigation graph and init details fragment

---
+++ app/src/main/java/dev/yash/keymanager/ui/details/GpgDetailsFragment.kt
@@ -0,0 +1,37 @@
+package dev.yash.keymanager.ui.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import app.yash.keymanager.databinding.GpgDetailsFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class GpgDetailsFragment : Fragment() {
+    private var _binding: GpgDetailsFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = GpgDetailsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val heading = binding.text1
+        val name = binding.text2
+
+        // TODO
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
