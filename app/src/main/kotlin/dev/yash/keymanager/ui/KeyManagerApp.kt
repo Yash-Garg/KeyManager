637ea572f7b610790f8f061e7c35c83f40568398
Yash-Garg <yash.garg@outlook.in>
2022-12-14 17:11:56 +0530

    feat(ui): update splash screen theming

--- app/src/main/kotlin/dev/yash/keymanager/ui/KeyManagerApp.kt
+++ app/src/main/kotlin/dev/yash/keymanager/ui/KeyManagerApp.kt
@@ -1,6 +1,10 @@
 package dev.yash.keymanager.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -12,32 +16,36 @@ import dev.yash.keymanager.ui.home.HomeScreen
 import dev.yash.keymanager.ui.home.HomeViewModel
 import dev.yash.keymanager.ui.theme.KeyManagerTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun KeyManagerApp() {
     val navController = rememberNavController()
 
     KeyManagerTheme {
-        NavHost(
-            navController = navController,
-            startDestination = NavDestinations.AuthScreen.route
-        ) {
-            composable(NavDestinations.AuthScreen.route) {
-                val authViewModel = hiltViewModel<AuthViewModel>()
-                AuthScreen(
-                    viewModel = authViewModel,
-                    onAuthNavigate = {
-                        navController.navigate(NavDestinations.HomeScreen.route) { popUpTo(0) }
-                    }
-                )
-            }
-            composable(NavDestinations.HomeScreen.route) {
-                val homeViewModel = hiltViewModel<HomeViewModel>()
-                HomeScreen(
-                    viewModel = homeViewModel,
-                    onLogoutNavigate = {
-                        navController.navigate(NavDestinations.AuthScreen.route) { popUpTo(0) }
-                    }
-                )
+        Scaffold {
+            NavHost(
+                modifier = Modifier.padding(it),
+                navController = navController,
+                startDestination = NavDestinations.AuthScreen.route
+            ) {
+                composable(NavDestinations.AuthScreen.route) {
+                    val authViewModel = hiltViewModel<AuthViewModel>()
+                    AuthScreen(
+                        viewModel = authViewModel,
+                        onAuthNavigate = {
+                            navController.navigate(NavDestinations.HomeScreen.route) { popUpTo(0) }
+                        }
+                    )
+                }
+                composable(NavDestinations.HomeScreen.route) {
+                    val homeViewModel = hiltViewModel<HomeViewModel>()
+                    HomeScreen(
+                        viewModel = homeViewModel,
+                        onLogoutNavigate = {
+                            navController.navigate(NavDestinations.AuthScreen.route) { popUpTo(0) }
+                        }
+                    )
+                }
             }
         }
     }
